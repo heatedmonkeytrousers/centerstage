670529988d52c2e93ecc5443040fb63a2afda8a5
Makayla McDonnell <makaylamcdonnell@gmail.com>
2023-12-22 19:40:06 -0600

    Hamster!

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AutonomousOpMode_Linear.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AutonomousOpMode_Linear.java
@@ -1,4 +1,8 @@
 package org.firstinspires.ftc.teamcode;
-
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+@Autonomous(name = "Robot: Automate left side", group = "Robot")
 public class AutonomousOpMode_Linear {
+    public AutonomousOpMode_Linear() {
+
+    }
 }
