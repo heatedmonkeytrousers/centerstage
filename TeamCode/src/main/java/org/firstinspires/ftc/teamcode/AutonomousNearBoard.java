c74f2f0772ade62ca55b1cd2a6a2e064c4e60ab2
Makayla McDonnell <makaylamcdonnell@gmail.com>
2024-01-04 11:08:35 -0600

    Far Autonomous Update

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AutonomousNearBoard.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AutonomousNearBoard.java
@@ -1,21 +1,28 @@
 package org.firstinspires.ftc.teamcode;
 import com.acmerobotics.roadrunner.geometry.Pose2d;
-import com.acmerobotics.roadrunner.geometry.Vector2d;
 import com.acmerobotics.roadrunner.trajectory.Trajectory;
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 
 import org.firstinspires.ftc.teamcode.drive.SampleMecanumDrive;
 
-@Autonomous(name="Autonomous: Far", group="Robot")
+@Autonomous(name="Autonomous: Near", group="Robot")
 
-public class AutonomousFar extends StandardSetupOpMode {
-    public AutonomousFar() {
+public class AutonomousNearBoard extends StandardSetupOpMode {
+    public AutonomousNearBoard() {
 
     }
 
     @Override
     public void runOpMode() throws InterruptedException {
         super.runOpMode();
+
+        // Initial parking spot
+        Motion.PARKING_SPOT parkingSpot = position;
+
+        // Determine parking spot
+        parkingSpot = position;
+        telemetry.addData("Parking Spot", parkingSpot);
+        telemetry.update();
 
         //If red alliance then yScale is -1.0
         //If blue alliance then yScale is 1.0
@@ -25,24 +32,19 @@ public class AutonomousFar extends StandardSetupOpMode {
         runtime.reset();
 
         SampleMecanumDrive drive = new SampleMecanumDrive(hardwareMap);
-
         super.arm.setShoulder(shoulder);
         super.wrist.start();
         super.claw.rightClose();
         super.claw.leftClose();
 
-        // Robot Poses
+        // Poses
         Pose2d startPose = new Pose2d();
         Pose2d dropPose = new Pose2d(16.5, 0, 0);
-        Pose2d avoidPose = new Pose2d(14,19, Math.toRadians(90 * yScale));
-        //Pose2d squarePose = new Pose2d();  figure our numbers for squaring with wall
-        Pose2d alignPose = new Pose2d(53,19, Math.toRadians(90 * yScale));
-        Pose2d backPose = new Pose2d(53,-85, Math.toRadians(90 * yScale));
-        Pose2d releasePose = new Pose2d(28,-85, Math.toRadians(90 * yScale));
-        Pose2d parkPose = new Pose2d(28,-85, Math.toRadians(-90 * yScale));
+        Pose2d boardPose = new Pose2d(25.5, 34 * yScale, Math.toRadians((360+(yScale*90)) % 360));
+        Pose2d parkPose = new Pose2d(4, 30 * yScale, Math.toRadians(-90 * yScale));
 
+        // Trajectories
         Trajectory start = drive.trajectoryBuilder(startPose)
-
                 .addTemporalMarker(0, () -> {
                     super.shoulder.setShoulderPosition(0.75, -200);
                 })
@@ -55,42 +57,24 @@ public class AutonomousFar extends StandardSetupOpMode {
                 })
                 .build();
 
-        Trajectory back = drive.trajectoryBuilder(dropPose)
-                .addTemporalMarker(0, () -> {
-                    super.arm.setArmPosition(1.0, 0);
-                    super.shoulder.setShoulderPosition(0.75, -100);
-                })
-                .lineToLinearHeading(avoidPose)
-                .build();
-
-        Trajectory forward1 = drive.trajectoryBuilder(avoidPose)
-                .lineToLinearHeading(alignPose)
-                .build();
-
-        Trajectory forward2 = drive.trajectoryBuilder(alignPose)
-                .lineToLinearHeading(backPose)
-                .build();
-
-        Trajectory board = drive.trajectoryBuilder(backPose)
-                .lineToLinearHeading(releasePose)
+        // Setting initial pose to startPose makes the robot avoid the drop areas
+        Trajectory board = drive.trajectoryBuilder(startPose)
                 .addTemporalMarker(0, () -> {
                     super.shoulder.setShoulderPosition(0.75, -739);
                 })
-                .addTemporalMarker(0, () -> {
-                    super.arm.setArmPosition(1, 1650);
-                })
+                .lineToSplineHeading(boardPose)
                 .addTemporalMarker(2, () -> {
                     super.claw.rightOpen();
                 })
                 .build();
 
 
-        Trajectory park = drive.trajectoryBuilder(releasePose)
+        Trajectory park = drive.trajectoryBuilder(boardPose)
                 .addTemporalMarker(0, () -> {
                     super.arm.setArmPosition(1, 0);
                 })
                 .addTemporalMarker(0.5, () -> {
-                    super.shoulder.setShoulderPosition(0.75, -40);
+                    super.shoulder.setShoulderPosition(0.75, 0);
                 })
                 .lineToSplineHeading(parkPose)
                 .build();
@@ -101,17 +85,11 @@ public class AutonomousFar extends StandardSetupOpMode {
         if(isStopRequested()) return;
 
         drive.followTrajectory(start);
-        sleep(1500);
-        drive.followTrajectory(back);
-        drive.followTrajectory(forward1);
-        drive.followTrajectory(forward2);
+        sleep(1000);
         drive.followTrajectory(board);
-        sleep(500);
+        sleep(200);
         drive.followTrajectory(park);
+        sleep(5000);
 
-
-        //sleep(1900);
-        //drive.followTrajectory(park);
-        //sleep(5000);
     }
 }
